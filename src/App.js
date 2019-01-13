32c8ec13fd25db1c19156b0123995eae9cbfd22a
romil.shah <romil.shah@pharmeasy.in>
2019-01-14 01:32:19 +0530

    The state property

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Romil', age: 21 },
+      { name: 'Mayank', age: 22 },
+      { name: 'Saurav', age: 27 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>  Hello Romil !</h1>
         <p>WELCOME TO REACT!</p>
-        <Person name="Romil" age="21" >My hobbies are: Dancing!</Person>
-        <Person name="Mayank" age="22">My hobbies are: NOTHING!</Person>
-        <Person name="Saurav" age="27" />
+        <button> Switch Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My hobbies are: Dancing!</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are: NOTHING!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
 
     );
