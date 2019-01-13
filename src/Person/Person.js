19c58d37a13a5c8f04db473861aca4f6ca4ff129
romil.shah <romil.shah@pharmeasy.in>
2019-01-14 00:31:32 +0530

    initial commit

---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const person = () => {
+	return <p> Romil M Shah vese efe lexi co and he is {Math.floor(Math.random() * 30)} years old </p>
+};
+
+
+export default person;
