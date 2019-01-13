fabf5d1806370d667276cbf1417d69f11165afdb
romil.shah <romil.shah@pharmeasy.in>
2019-01-14 01:46:59 +0530

    Working with state property

--- src/App.js
+++ src/App.js
@@ -12,12 +12,24 @@ class App extends Component {
     ]
   }
 
+  SwitchNameHandler = () => {
+
+    this.setState({
+
+      persons: [
+        { name: 'Romil Shah', age: 22 },
+        { name: 'Mayank Mehta', age: 22 },
+        { name: 'Saurav', age: 28 }
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>  Hello Romil !</h1>
         <p>WELCOME TO REACT!</p>
-        <button> Switch Name </button>
+        <button onClick={this.SwitchNameHandler}> Switch Name </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My hobbies are: Dancing!</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are: NOTHING!</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
